180f3484966a8941dae5fcdf183d151761ccc8ad
SirIcy <hk.web@web.de>
2023-01-21 12:10:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ unzip main.zip
 mv venus.dbus-smartmeter-main /data/dbus-smartmeter
 chmod 755 /data/dbus-smartmeter/service/run
 chmod 744 /data/dbus-smartmeter/kill_me.sh
-FILE=/data/rc.local
-if ; then
-echo "ln -s /data/dbus-smartmeter/service /service/dbus-smartmeter" >> /data/rc.local
+if [ -f /data/rc.local ];
+then
+ echo "ln -s /data/dbus-smartmeter/service /service/dbus-smartmeter" >> /data/rc.local
 else
-echo "#!/bin/bash\nln -s /data/dbus-smartmeter/service /service/dbus-smartmeter" >> /data/rc.local
+ echo -e "#!/bin/bash\nln -s /data/dbus-smartmeter/service /service/dbus-smartmeter" > /data/rc.local
+ chmod 755 /data/rc.local
 fi
